767866a793d074572ddca6f6dabfd4d182caedb8
Pontus <pontuspersson@outlook.com>
2021-03-08 16:25:24 +0100

    small fixed
    npm build etc.

---
+++ build/precache-manifest.4b08856e6cfaf70c4743d39c598e897d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963e72eb2d7e74af2185aca4a887e171",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75c9e75fdc5a11fea9fd",
+    "url": "/static/css/2.2b73047c.chunk.css"
+  },
+  {
+    "revision": "75c9e75fdc5a11fea9fd",
+    "url": "/static/js/2.6c874a9b.chunk.js"
+  },
+  {
+    "revision": "ca2abe649ff858defa21eaf931086938",
+    "url": "/static/js/2.6c874a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a867d60f6b614dc829",
+    "url": "/static/js/main.84d7093e.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "ba3a71893d5df6ed9fdf17b12f39c273",
+    "url": "/static/media/emptyCartGuy.ba3a7189.jpeg"
+  },
+  {
+    "revision": "25ce7bd0b997cb6d621934e3e8c685fd",
+    "url": "/static/media/logo.25ce7bd0.png"
+  }
+]);
